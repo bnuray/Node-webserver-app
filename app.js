31c8e92df197b82c42a00e46c4a7e64eb2ee157d
Nuray Bactisa <nuraybactisa@yahoo.com>
2021-02-19 09:57:41 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http'); //http ne permite sa cream un webserver
+const server = http.createServer((req,res)=>{ //req = request (what the client requests from us)
+    if(req.url === '/') {
+        var date = new Date();
+        var current_minute = date.getMinutes();
+        var current_hour = date.getHours();
+        res.write(`Salut, ai accesat server-ul la ora ${current_hour}:${current_minute}`);
+        res.end();
+    } else {
+        res.write('Nu ai nimerit unde trebuie');
+        res.end();
+    }
+})
+server.listen('4500');
